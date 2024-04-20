7be3e3b10af2a816a9d04874b63137fd3f46f79a
Chris Bowen <4783294+cbbowen@users.noreply.github.com>
2024-04-20 00:54:01 -0700

    Initial version.

---
+++ README.md
@@ -0,0 +1,44 @@
+Efficient undo history for non-invertible actions.
+
+[Documentation](https://cbbowen.github.io/history)
+
+# Motivation
+
+An undo history can be implemented as a stack of application states, where each time the state changes, a new copy is pushed onto the stack. Undo is then as simple as popping the most recent state off the stack. The disadvantage of this approach is that it requires storing *O*(n) application states where n is the length of the undo history.
+
+That's often prohibitive, so an undo history is commonly implemented instead as stack of actions applied to the application state, each of which must be invertible. This is the [command pattern](https://en.wikipedia.org/wiki/Command_pattern). Undo can then be implemented by popping the top action from the stack and applying its inverse to the application state[^1]. This still requires storing *O*(n) actions, but actions are typically _much_ smaller. The disadvantage of this approach is that it requires actions to be invertible. Often, this inverse must be manually implemented for each type of action.
+
+This library provides a middle-ground. It stores *O*(log n) states while still allowing pushing and popping actions in *O*(1) (amortized) time and a single action application, all without requiring implementation of action inverses. Furthermore, it also allows reconstructing the state at any point in the history in *O*(k + log n) time[^2].
+
+[^1]: Observe that storing a stack of states is a special case of storing a stack of actions where each action is a replacement of the entire state.
+
+[^2]: It is possible to get rid of the log n term in the running time of `get_state` but it's not generally relevant to the undo history application. If you have a use-case where it is, let me know.
+
+# Usage
+
+Implement the [`Action`] trait for your action type, add them to the history with [`History::push_action`], and retrieve the current stat with [`History::current_state`]. Remove the most recent action with [`History::pop_action`].
+
+```rust
+enum MyAction {
+	Add(i32),
+	Sub(i32),
+}
+
+impl history::Action for MyAction {
+	type State = i32;
+	fn apply(&self, state: i32) -> i32 {
+		match self {
+			MyAction::Add(a) => state + a,
+			MyAction::Sub(a) => state - a,
+		}
+	}
+}
+
+let mut history = history::History::default();
+history.push_action(MyAction::Add(710));
+assert_eq!(*history.current_state(), 710);
+history.push_action(MyAction::Sub(42));
+assert_eq!(*history.current_state(), 668);
+history.pop_action();
+assert_eq!(*history.current_state(), 710);
+```
---
+++ src/lib.rs
@@ -0,0 +1,309 @@
+#![forbid(unsafe_code)]
+#![doc = include_str!("../README.md")]
+
+use thiserror::Error;
+
+/// An action which can be added to a [`History`].
+pub trait Action {
+    /// The type of state this action affects.
+    type State: Clone;
+
+    /// Applies this action to a state, producing a new state.
+    fn apply(&self, state: Self::State) -> Self::State;
+}
+
+/// Identifies a specific version in the history.
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Version(usize);
+
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum HistoryError {
+    #[error("version out of range")]
+    VersionOutOfRange(Version),
+}
+
+/// The history of state as actions are applied to it.
+#[derive(Debug, Clone)]
+pub struct History<A: Action> {
+    actions: Vec<A>,
+    states: Vec<(Version, A::State)>,
+}
+
+impl<A: Action> Default for History<A>
+where
+    A::State: Default,
+{
+    fn default() -> Self {
+        Self::new(Default::default())
+    }
+}
+
+impl<A: Action> History<A> {
+    /// Constructs a new history with the given initial state.
+    pub fn new(initial: A::State) -> Self {
+        Self {
+            actions: Vec::new(),
+            states: [(Version::default(), initial)].into_iter().collect(),
+        }
+    }
+
+    /// Returns all the actions from oldest to newest.
+    pub fn actions(&self) -> impl Iterator<Item = &A> {
+        self.actions.iter()
+    }
+
+    /// Returns all the versions from oldest to newest.
+    pub fn versions(&self) -> impl Iterator<Item = Version> {
+        (0..=self.actions.len()).map(Version)
+    }
+
+    /// Adds a new action to the end of the history and returns the new version.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use history::*;
+    /// # #[derive(Debug, PartialEq, Eq)]
+    /// # struct Add(i32);
+    /// # impl Action for Add {
+    /// #  type State = i32;
+    /// #  fn apply(&self, state: i32) -> i32 { self.0 + state }
+    /// # }
+    /// # let mut history = History::default();
+    /// let before = *history.current_state();
+    /// assert_eq!(*history.current_state(), 0);
+    /// history.push_action(Add(42));
+    /// assert_eq!(*history.current_state(), 42);
+    /// ```
+    ///
+    /// # Time complexity
+    ///
+    /// Takes *O*(1) amortized time.
+    pub fn push_action(&mut self, action: A) -> Version {
+        let new_state = action.apply(self.current_state().clone());
+        self.actions.push(action);
+        let new_version = Version(self.actions.len());
+
+        // Restore invariants.
+        let num_states = self.states.len();
+        let index_to_remove =
+            num_states as isize - (1 + (new_version.0 + 1).trailing_zeros()) as isize;
+        debug_assert!(index_to_remove > 0 || index_to_remove == -1);
+        if index_to_remove > 0 {
+            // This takes `O(k)` time but `k` is exponentially-distributed, so amortized, it is
+            // `O(1)`.
+            self.states.remove(index_to_remove as usize);
+        }
+        self.states.push((new_version, new_state));
+        new_version
+    }
+
+    /// Removes and returns the most recent action or `None` if there are no actions.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use history::*;
+    /// # #[derive(Debug, PartialEq, Eq)]
+    /// # struct Add(i32);
+    /// # impl Action for Add {
+    /// #  type State = i32;
+    /// #  fn apply(&self, state: i32) -> i32 { self.0 + state }
+    /// # }
+    /// # let mut history = History::default();
+    /// let before = *history.current_state();
+    /// history.push_action(Add(42));
+    /// assert_eq!(*history.current_state(), 42);
+    /// assert_eq!(history.pop_action(), Some(Add(42)));
+    /// assert_eq!(*history.current_state(), 0);
+    /// ```
+    ///
+    /// # Time complexity
+    ///
+    /// Takes *O*(1) amortized time.
+    pub fn pop_action(&mut self) -> Option<A> {
+        let removed_version = self.actions.len();
+        let action = self.actions.pop()?;
+        self.states.pop();
+
+        let index_to_insert =
+            self.states.len() as isize - (removed_version + 1).trailing_zeros() as isize;
+        if index_to_insert > 0 {
+            let index_to_insert = index_to_insert as usize;
+            let version_to_insert =
+                removed_version + 1 - (1 << (1 + self.states.len() - index_to_insert));
+            let (recent_version, state) = self.get_cached_state(index_to_insert - 1);
+            let state = self.actions[recent_version.0..version_to_insert]
+                .iter()
+                .fold(state.clone(), |s, a| a.apply(s));
+            self.states
+                .insert(index_to_insert, (Version(version_to_insert), state));
+        }
+        Some(action)
+    }
+
+    /// Returns the current version.
+    pub fn current_version(&self) -> Version {
+        Version(self.actions.len())
+    }
+
+    fn get_cached_state(&self, state_index: usize) -> (Version, &A::State) {
+        let (version, state) = self.states.get(state_index).unwrap();
+        (*version, state)
+    }
+
+    /// Returns the most recent cached state not after the given `version` or an error if no such
+    /// cached state exists.
+    fn get_recent_state_index(&self, version: Version) -> Result<usize, HistoryError> {
+        if version > self.current_version() {
+            return Err(HistoryError::VersionOutOfRange(version));
+        }
+        let index = self.states.partition_point(|(v, _)| *v <= version) - 1;
+        Ok(index)
+    }
+
+    /// Returns the state at the current version.
+    pub fn current_state(&self) -> &A::State {
+        let (version, state) = self.states.last().unwrap();
+        debug_assert_eq!(*version, self.current_version());
+        state
+    }
+
+    /// Returns the state at the specified version.
+    pub fn get_state(&self, version: Version) -> Result<A::State, HistoryError> {
+        let state_index = self.get_recent_state_index(version)?;
+        let (recent_version, state) = self.get_cached_state(state_index);
+        Ok(self.actions[recent_version.0..version.0]
+            .iter()
+            .fold(state.clone(), |s, a| a.apply(s)))
+    }
+
+    /// Returns the versions of the cached states.
+    #[cfg(test)]
+    pub(crate) fn cached_versions(&self) -> Vec<Version> {
+        self.states
+            .iter()
+            .map(|(version, _)| version)
+            .cloned()
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // `proptest-derive`'s derived `Arbitrary` produces this warning.
+    #![allow(non_local_definitions)]
+
+    use super::*;
+    use proptest::prelude::*;
+    use proptest_derive::Arbitrary;
+
+    #[derive(Arbitrary, Clone, Copy, Debug, PartialEq, Eq)]
+    struct TestAction(u8);
+
+    impl Action for TestAction {
+        type State = Vec<u8>;
+
+        fn apply(&self, mut state: Self::State) -> Self::State {
+            state.push(self.0);
+            state
+        }
+    }
+
+    #[derive(Arbitrary, Clone, Debug)]
+    enum Step {
+        Push(TestAction),
+        Pop,
+    }
+
+    prop_compose! {
+        fn history_strategy()(steps: Vec<Step>) -> History<TestAction> {
+             let mut history = History::default();
+             for step in steps {
+                match step {
+                    Step::Push(action) => {
+                         history.push_action(action);
+                    }
+                    Step::Pop => {
+                         history.pop_action();
+                    }
+                }
+             }
+             history
+        }
+    }
+
+    impl Arbitrary for History<TestAction> {
+        type Parameters = ();
+        type Strategy = BoxedStrategy<Self>;
+        fn arbitrary_with(_args: Self::Parameters) -> Self::Strategy {
+            history_strategy().boxed()
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn push_action(mut history: History<TestAction>, action: TestAction) {
+             let previous_version = history.current_version();
+             let mut actions = Vec::from_iter(history.actions().cloned());
+             let new_version = history.push_action(action.clone());
+
+             // This test only verifies the actions and current version are updated appropriately, not the states. The consistency of the states with the actions is covered by other tests.
+             prop_assert_eq!(history.current_version(), new_version);
+             prop_assert!(new_version > previous_version);
+             actions.push(action);
+             prop_assert_eq!(Vec::from_iter(history.actions().cloned()), actions);
+        }
+
+        fn pop_action(mut history: History<TestAction>) {
+             let previous_version = history.current_version();
+             let mut actions = Vec::from_iter(history.actions().cloned());
+             let action = history.pop_action();
+
+             // This test only verifies the actions and current version are updated appropriately, not the states. The consistency of the states with the actions is covered by other tests.
+             prop_assert_eq!(action, actions.pop());
+             prop_assert!(history.current_version() <= previous_version);
+             prop_assert!(action.is_none() || history.current_version() < previous_version);
+             prop_assert_eq!(Vec::from_iter(history.actions().cloned()), actions);
+        }
+
+        #[test]
+        fn get_state(history: History<TestAction>) {
+             let mut state = Default::default();
+             let mut version_iter = history.versions();
+             let Some(initial_version) = version_iter.next() else {
+                prop_assert!(false);
+                return Ok(());
+             };
+             let actual_state = history.get_state(initial_version);
+             prop_assert_eq!(actual_state.as_ref(), Ok(&state));
+             for (version, action) in version_iter.zip(history.actions()) {
+                state = action.apply(state);
+                let actual_state = history.get_state(version);
+                prop_assert_eq!(actual_state.as_ref(), Ok(&state));
+             }
+        }
+    }
+
+    #[test]
+    fn example() {
+        let mut history = History::default();
+        println!("{:?}", history.cached_versions());
+
+        const N: usize = 15;
+
+        for i in 1..=N {
+            history.push_action(TestAction((i % 256) as u8));
+            println!("{:?}", history.cached_versions());
+        }
+
+        for version in history.versions() {
+            println!("{:?}", history.get_state(version));
+        }
+
+        for _ in 1..=N {
+            history.pop_action();
+            println!("{:?}", history.cached_versions());
+        }
+    }
+}
